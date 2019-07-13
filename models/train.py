b474ec14593546c3ac1b3126b5a39dcc6404e994
Michael Zheng <michaelzheng@pocketgems.com>
2019-07-13 13:00:48 -0700

    train from checkpoint

--- models/train.py
+++ models/train.py
@@ -124,14 +124,14 @@ def optimize(style_name, style_path, epochs, batch_size, learning_rate, style_w,
 
         saver = tf.train.Saver()
         sess.run(tf.global_variables_initializer())
-        # if os.path.isdir(checkpoint_path):
-        #     ckpt = tf.train.get_checkpoint_state(checkpoint_path)
-        #     if ckpt and ckpt.model_checkpoint_path:
-        #         saver.restore(sess, ckpt.model_checkpoint_path)
-        #     else:
-        #         raise Exception("No Checkpoint Found")
-        # else:
-        #     saver.restore(sess, checkpoint_path)
+        if os.path.isdir(checkpoint_path):
+            ckpt = tf.train.get_checkpoint_state(checkpoint_path)
+            if ckpt and ckpt.model_checkpoint_path:
+                saver.restore(sess, ckpt.model_checkpoint_path)
+            else:
+                raise Exception("No Checkpoint Found")
+        else:
+            saver.restore(sess, checkpoint_path)
         for i in range(epochs):
             start_time = time.time()
             for batch in image_generator('../data/train2014', batch_size):
@@ -169,7 +169,7 @@ if __name__ == '__main__':
 
     style_image_path = '../images/style/mosaic_dots.jpg'
     optimize('mosaic', style_image_path, 1, 4, 1e-3,
-             style_w, content_w, tv_w, 1000, 'checkpoints',
-             'puppy', '../images/content/puppy.jpg', 1000, debug=False)
+             style_w, content_w, tv_w, 2000, 'checkpoints/mosaic_checkpoint/new_checkpoints',
+             'puppy', '../images/content/puppy.jpg', 2000, debug=False)
 
 
